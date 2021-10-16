dc417b38c3a40cdc6687b927f1749824fd3bdfd6
Pierce <pierce@vulpes.dev>
2021-10-16 01:38:34 -0400

    dont use markup, increase text visibility

--- lua/autorun/client/simple_weapon_pickup_init.lua
+++ lua/autorun/client/simple_weapon_pickup_init.lua
@@ -12,7 +12,7 @@ surface.CreateFont( "CFC_WeaponPickupPrompt", {
     symbol = false,
     rotary = false,
     shadow = true,
-    additive = true,
+    additive = false,
     outline = true,
 } )
 local pickupDistance = 110
@@ -35,8 +35,14 @@ local function drawHalos()
 end
 hook.Add( "PreDrawHalos", "SimpleWeaponPickup_DrawHalos", drawHalos )
 
+local text = {
+    {text="[E]", color=Color(255,0,0)},
+    {text=" ",  color=Color(255,255,255)},
+    {text="to pick up", color=Color(255,255,255)},
+    {text=" ",  color=Color(255,255,255)},
+    {text="weapon_name", color=Color(0,95,212)}
+}
 
-local weaponTextMarkup = markup.Parse("<font=CFC_WeaponPickupPrompt><colour=255,0,0,255>[E]</colour> to pick up <colour=0,95,212,255>weapon_name</colour></font>")
 local function doHUDPaint() 
     if not LocalPlayer():Alive() then return end
     if LocalPlayer():GetObserverMode() ~= OBS_MODE_NONE then return end
@@ -45,13 +51,24 @@ local function doHUDPaint()
 
     if EyePos():Distance(ent:GetPos()) > pickupDistance then return end
     local printName = LANG.TryTranslation(ent:GetPrintName() or ent.PrintName or "...")
-    
+
     surface.SetFont( "CFC_WeaponPickupPrompt" )
-    local w, _ = surface.GetTextSize(printName)
-    weaponTextMarkup.blocks[3].text = printName
-    weaponTextMarkup.totalWidth = weaponTextMarkup.totalWidth + w - weaponTextMarkup.blocks[3].thisX 
-    weaponTextMarkup.blocks[3].thisX = w
-    
-    weaponTextMarkup:Draw(ScrW()/2, ScrH()/2+50, TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+    local totalWidth = 0
+    text[5].text = printName
+    for  _, v in ipairs(text) do
+        local width, height = surface.GetTextSize( v.text )
+        v.width = width
+        totalWidth = totalWidth + width
+    end
+
+    local y = ScrH()/2 +50
+    local x = ScrW()/2 - totalWidth/2
+    for  _, v in ipairs(text) do
+        draw.SimpleText(v.text, "CFC_WeaponPickupPrompt", x+1, y+1, COLOR_BLACK, TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER)
+        draw.SimpleText(v.text, "CFC_WeaponPickupPrompt", x, y, v.color, TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER)
+        
+        x = x + v.width
+    end
 end
+
 hook.Add( "HUDPaint", "SimpleWeaponPickup_PaintHud", doHUDPaint)
